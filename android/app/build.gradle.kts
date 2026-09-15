0e908fe373f870c85bcf8633fc6ee92378e7c9fb
hmgtawfiq <hmg.tawfiq@gmail.com>
2026-09-15 20:45:13 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,4 +37,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
 }
